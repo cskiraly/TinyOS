46eed14750a239d77a22eb40cebd2905042f692d
jpolastre <jpolastre@285f258d-8064-b571-fbc6-9656dc998e90>
2005-01-20 22:03:26 +0000

    added "FAIL" to the error codes to allow existing code to still work and use "FAIL" as an "unknown error has occurred".  May be changed in the future, just currently being committed to get other code to work.
    git-svn-id: http://tinyos-main.googlecode.com/svn@40 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/types/TinyError.h
+++ branches/tinyos-2_0_devel-BRANCH/tos/types/TinyError.h
@@ -1,5 +1,35 @@
+// $Id: TinyError.h,v 1.1.2.4 2005-01-20 22:03:26 jpolastre Exp $
+/*									tab:4
+ * "Copyright (c) 2000-2005 The Regents of the University  of California.  
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, without fee, and without written agreement is
+ * hereby granted, provided that the above copyright notice, the following
+ * two paragraphs and the author appear in all copies of this software.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
+ * CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
+ * ON AN "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO
+ * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS."
+ */
+
+/**
+ * @author Phil Levis
+ * Revision:  $Revision: 1.1.2.4 $
+ *
+ * Defines global error codes for error_t in TinyOS.
+ */
+
 typedef enum {
   SUCCESS        = 0,
-  ESIZE          = 1,           // Parameter passed in was too big.
-  ECANCEL        = 2,           // Operation cancelled by a call.
+  FAIL           = 1,        // An unknown error caused the operation to fail
+  ESIZE          = 2,        // Parameter passed in was too big.
+  ECANCEL        = 3,        // Operation cancelled by a call.
 } error_t;
