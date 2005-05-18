dd8a1a83ae9cc4628b235e9cee79d8c17a8ad3bd
jpolastre <jpolastre@285f258d-8064-b571-fbc6-9656dc998e90>
2005-05-18 05:19:59 +0000

    define the bcast and uart addresses here, but it should probably go somewhere else
    git-svn-id: http://tinyos-main.googlecode.com/svn@275 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/types/TOSMsg.h
+++ branches/tinyos-2_0_devel-BRANCH/tos/types/TOSMsg.h
@@ -7,6 +7,14 @@
 #define TOSH_DATA_LENGTH 29
 #endif
 
+#ifndef TOS_BCAST_ADDR
+#define TOS_BCAST_ADDR 0xFFFF
+#endif
+
+#ifndef TOS_UART_ADDR
+#define TOS_UART_ADDR 0x7E
+#endif
+
 typedef nx_struct message_t {
   TOSRadioHeader header;
   nx_uint8_t data[TOSH_DATA_LENGTH];
