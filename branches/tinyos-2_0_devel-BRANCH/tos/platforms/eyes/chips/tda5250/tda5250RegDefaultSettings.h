5dfbfd3605e9c9ecb92f448b6b24a4cb40bd1e41
klueska <klueska@285f258d-8064-b571-fbc6-9656dc998e90>
2005-05-24 16:19:01 +0000

    Renamed tda5250RegDefaults.h to tda5250RegDefaultSettings.h
    git-svn-id: http://tinyos-main.googlecode.com/svn@332 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/platforms/eyes/chips/tda5250/tda5250RegDefaultSettings.h
+++ branches/tinyos-2_0_devel-BRANCH/tos/platforms/eyes/chips/tda5250/tda5250RegDefaultSettings.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004, Technische Universitaet Berlin
+ * Copyright (c) 2004, Technische Universitat Berlin
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,7 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name of the Technische Universitaet Berlin nor the names
+ * - Neither the name of the Technische Universitat Berlin nor the names
  *   of its contributors may be used to endorse or promote products derived
  *   from this software without specific prior written permission.
  *
@@ -25,32 +25,35 @@
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * - Description ---------------------------------------------------------
- * Macros for setting the default register values in the TDA5250.
- * - Revision ------------------------------------------------------------
+ * - Revision -------------------------------------------------------------
  * $Revision: 1.1.2.1 $
- * $Date: 2005-05-20 12:54:14 $
- * Author: Kevin Klues (klues@tkn.tu-berlin.de)
- * ========================================================================
+ * $Date: 2005-05-24 16:19:01 $ 
+ * ======================================================================== 
+ */
+ 
+ /**
+ * tda5250RegDefaultSettings Header File
+ * Defines the default values of the registers for the TDA5250 Radio
+ *
+ * @author Kevin Klues (klues@tkn.tu-berlin.de)
  */
 
-#ifndef HPLTDA5250REGDEFAULTS_H
-#define HPLTDA5250REGDEFAULTS_H
+#ifndef HPLTDA5250REGDEFAULTSETTINGS_H
+#define HPLTDA5250REGDEFAULTSETTINGS_H
 
 // Default values of data registers
-#define TDA5250_DATA_CONFIG_DEFAULT           0x04F9
-#define TDA5250_DATA_FSK_DEFAULT              0x0A0C
-#define TDA5250_DATA_XTAL_TUNING_DEFAULT      0x0012
-#define TDA5250_DATA_LPF_DEFAULT              0x5A
-#define TDA5250_DATA_ON_TIME_DEFAULT          0xFEC0
-#define TDA5250_DATA_OFF_TIME_DEFAULT         0xF380
-#define TDA5250_DATA_COUNT_TH1_DEFAULT        0x0000
-#define TDA5250_DATA_COUNT_TH2_DEFAULT        0x0001
-#define TDA5250_DATA_RSSI_TH3_DEFAULT         0xFF
-#define TDA5250_DATA_CLK_DIV_DEFAULT          0x08
-#define TDA5250_DATA_XTAL_CONFIG_DEFAULT      0x01
-#define TDA5250_DATA_BLOCK_PD_DEFAULT         0xFFFF
+#define TDA5250_REG_DEFAULT_SETTING_CONFIG           0x04F9
+#define TDA5250_REG_DEFAULT_SETTING_FSK              0x0A0C
+#define TDA5250_REG_DEFAULT_SETTING_XTAL_TUNING      0x0012
+#define TDA5250_REG_DEFAULT_SETTING_LPF              0x5A
+#define TDA5250_REG_DEFAULT_SETTING_ON_TIME          0xFEC0
+#define TDA5250_REG_DEFAULT_SETTING_OFF_TIME         0xF380
+#define TDA5250_REG_DEFAULT_SETTING_COUNT_TH1        0x0000
+#define TDA5250_REG_DEFAULT_SETTING_COUNT_TH2        0x0001
+#define TDA5250_REG_DEFAULT_SETTING_RSSI_TH3         0xFF
+#define TDA5250_REG_DEFAULT_SETTING_CLK_DIV          0x08
+#define TDA5250_REG_DEFAULT_SETTING_XTAL_CONFIG      0x01
+#define TDA5250_REG_DEFAULT_SETTING_BLOCK_PD         0xFFFF
 
-#endif //HPLTDA5250REGDEFAULTS_H
+#endif //HPLTDA5250REGDEFAULTSETTINGS_H
 
