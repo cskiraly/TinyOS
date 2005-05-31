943347d6ad9d3810d040196aa6225b29c8faf22f
janhauer <janhauer@285f258d-8064-b571-fbc6-9656dc998e90>
2005-05-31 00:10:08 +0000

    HAL1 for ADC12 according to TEP 101.
    git-svn-id: http://tinyos-main.googlecode.com/svn/branches/tinyos-2_0_devel-BRANCH@364 285f258d-8064-b571-fbc6-9656dc998e90

--- tos/chips/msp430/adc12/MSP430ADC12.h
+++ tos/chips/msp430/adc12/MSP430ADC12.h
@@ -1,11 +1,73 @@
+/* 
+ * Copyright (c) 2004, Technische Universitaet Berlin
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - Neither the name of the Technische Universitaet Berlin nor the names
+ *   of its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * - Revision -------------------------------------------------------------
+ * $Revision: 1.1.2.2 $
+ * $Date: 2005-05-31 00:10:08 $
+ * @author: Jan Hauer <hauer@tkn.tu-berlin.de>
+ * ========================================================================
+ */
+ 
 #ifndef MSP430ADC12_H
 #define MSP430ADC12_H
+#include "RefVoltGenerator.h"
+
+typedef enum
+{
+   MSP430ADC12_SUCCESS,            // conversion started successfully 
+   MSP430ADC12_DELAYED,            // conversion will start when VREF high (max 17ms)
+   MSP430ADC12_FAIL_NOT_RESERVED,  // error: client has not reserved (request failed)
+   MSP430ADC12_FAIL_VREF,          // VREF in use at different voltage level (request failed)
+   MSP430ADC12_FAIL_JIFFIES,       // jiffies out of bounds (request failed)
+   MSP430ADC12_FAIL_LENGTH,        // length value illegal (request failed)
+   MSP430ADC12_FAIL_BUSY,          // request already pending for client (second request failed)
+} msp430adc12_result_t;
+
+typedef struct 
+{
+  unsigned int refVolt2_5: 1;         // reference voltage level
+  unsigned int clockSourceSHT: 2;     // clock source sample-hold-time
+  unsigned int clockSourceSAMPCON: 2; // clock source sampcon signal
+  unsigned int clockDivSAMPCON: 2;    // clock divider sampcon
+  unsigned int referenceVoltage: 3;   // reference voltage
+  unsigned int clockDivSHT: 3;        // clock divider sample-hold-time
+  unsigned int inputChannel: 4;       // input channel
+  unsigned int sampleHoldTime: 4;     // sample-hold-time
+  unsigned int : 0;                   // aligned to a word boundary 
+} msp430adc12_channel_config_t;
 
 /*
- * An application using the MSP430ADC12Single or MSP430ADC12Multiple interface 
- * needs to call bind() to bind the interface instance to certain settings which 
- * will be used for all subsequent conversions. In the following a macro,
- * ADC12_SETTINGS(), is provided to conveniently create the parameter for bind.
+ * When the MSP430ADC12SingleChannel.getConfigurationData() event is 
+ * signalled an application must return the settings for the channel
+ * it wants to sample as a msp430adc12_channel_config_t. 
+ * For convienience a macro, ADC12_SETTINGS, is provided below that
+ * allows a msp430adc12_channel_config_t to be defined as
+ * constant (e.g. to be placed in hardware.h or sensorboard file):
  * The macro takes 8 arguments:
  * 
  * ADC12_SETTINGS(IC, RV, SHT, CS_SHT, CD_SHT, CS_SAMPCON, CD_SAMPCON, RVL)
@@ -14,19 +76,18 @@
  * 
  * IC: Input channel to be sampled. An (external) input channel maps to one of
  * msp430's pins (see device specific data sheet which pin it is mapped to, A0-A7).
- * Make sure this pin is set to module function and input when sampling the
- * channel. Use inputChannel_enum in MSP430ADC12.h for IC.
+ * The HAL1 will take care of configuring the corresponding pin automatically 
+ * (set it to module function and input). Use inputChannel_enum (below) for IC.
  * 
  * RV: Reference voltage for the conversion(s). The MSP430 allows conversions with
- * VREF+ as a stable reference voltage (see RefVolt component). Therefore the
- * HAL module checks each request for the reference voltage and if necessary
- * switches on VREF+ automatically (via RefVolt component).  However, because there
- * is a startup delay of max. 17ms for the reference voltage generator to become
+ * VREF+ as a stable reference voltage (see RefVoltGenerator component). Therefore the
+ * HAL1 module checks each request for the reference voltage and if necessary
+ * switches on VREF+ automatically (via RefVoltGenerator component).  Because there
+ * is a startup delay of 17ms for the reference voltage generator to become
  * stable, a call getData() might result in an event dataReady() delayed by 17ms.
  * To avoid this, the application can start the reference voltage generator itself
- * by calling RefVolt.get() 17ms prior to the first conversion (and call
- * RefVolt.release() after the last conversion). Use referenceVoltage_enum in
- * MSP430ADC12.h for RV.
+ * (via RefVoltGenerator component) 17ms prior to the first conversion. 
+ * Use referenceVoltage_enum (below) for RV.
  * 
  * SHT: Sample-hold-time. This defines the time a channel is actually sampled for.  It
  * is measured in clock cycles of the sample-hold clock source (CS_SHT) and depends
@@ -57,68 +118,43 @@
  * it is ignored. It specifies the level of VREF+. Use refVolt2_5_enum for RVL.
  * 
  * EXAMPLE:
- * Binding to settings for a sensor on channel A1, using 
+ * Settings for a sensor on channel A1, using 
  * reference voltage VR+ = VREF+ and VR­ = AVSS, a sample hold-time
  * of 4 * (1/5000000) * 2 = 1.6us, a sampcon source of SMCLK and
  * reference voltage generator level of 1.5 Volt:
  *
- * call MSP430ADC12Multiple.bind(ADC12_SETTINGS(INPUT_CHANNEL_A1, 
- *                                              REFERENCE_VREFplus_AVss,
- *                                              SAMPLE_HOLD_4_CYCLES,
- *                                              SHT_SOURCE_ADC12OSC,
- *                                              SHT_CLOCK_DIV_2,
- *                                              SAMPCON_SOURCE_SMCLK,
- *                                              SAMPCON_CLOCK_DIV_1,
- *                                              REFVOLT_LEVEL_1_5));
+ * // in hardware.h:
+ * #define CHANNEL_A1_SETTINGS ADC12_SETTINGS(INPUT_CHANNEL_A1, \
+ *                                              REFERENCE_VREFplus_AVss, \
+ *                                              SAMPLE_HOLD_4_CYCLES, \
+ *                                              SHT_SOURCE_ADC12OSC, \
+ *                                              SHT_CLOCK_DIV_2, \
+ *                                              SAMPCON_SOURCE_SMCLK, \
+ *                                              SAMPCON_CLOCK_DIV_1, \
+ *                                              REFVOLT_LEVEL_1_5))
+ *  
+ * // in application
+ * async event msp430adc12_channel_config_t MSP430ADC12SingleChannel.getConfigurationData()
+ * {
+ *   return CHANNEL_A1_SETTINGS;
+ * }
  * 
  * Now the following command takes 100 samples, each separated by 1ms 
  * (assuming SMCLK runs at 1MHz):
  *
- * call MSP430ADC12Multiple.getData(buf, 100, 1000);
+ * call MSP430ADC12SingleChannel.getMultipleData(buf, 100, 1000);
  *
  * After 17ms (initial start of reference voltage generator) + 100 * 1 ms
- * = 117 ms the corresponding event is signalled.
+ * = 117 ms the multipleDataReady() event is signalled.
  * Note that the SAMPCON signal is concurrent to the sampling process,
  * i.e. sample-hold-time is not relevant for the calculation of the 117ms.
- *
- * In the configuration the MSP430ADC12Single and MSP430ADC12Multiple
- * need to be instantiated with unique("MSP430ADC12").
  */
 #define ADC12_SETTINGS(IC, RV, SHT, CS_SHT, CD_SHT, CS_SAMPCON, CD_SAMPCON, RVL) \
         (int2adcSettings(((((((((((((((((uint32_t) SHT) << 4) + IC) << 3) \
         + CD_SHT) << 3) + RV) << 2) + CD_SAMPCON) << 2) + CS_SAMPCON) << 2) \
         + CS_SHT) << 1) + RVL)))
 
-/*
- * Defining the 'adcPort' variable in
- * ADCControl.bindPort(uint8_t port, uint8_t adcPort).
- *
- * IC:  member of inputChannel_enum
- * RV:  member of referenceVoltage_enum
- * RVL: member of refVolt2_5_enum
- */
-#define ASSOCIATE_ADC_CHANNEL(IC, RV, RVL) \
-        ((((RVL << 3) + RV) << 4) + IC)
-        
-typedef struct 
-{
-  unsigned int refVolt2_5: 1;         // reference voltage level
-  unsigned int clockSourceSHT: 2;     // clock source sample-hold-time
-  unsigned int clockSourceSAMPCON: 2; // clock source sampcon signal
-  unsigned int clockDivSAMPCON: 2;    // clock divider sampcon
-  unsigned int referenceVoltage: 3;   // reference voltage
-  unsigned int clockDivSHT: 3;        // clock divider sample-hold-time
-  unsigned int inputChannel: 4;       // input channel
-  unsigned int sampleHoldTime: 4;     // sample-hold-time
-  unsigned int : 0;                   // aligned to a word boundary 
-} MSP430ADC12Settings_t;
  
-typedef enum
-{
-   MSP430ADC12_FAIL = 0,
-   MSP430ADC12_SUCCESS = 1,
-   MSP430ADC12_DELAYED = 2,
-} msp430ADCresult_t;
 
 enum refVolt2_5_enum
 {
@@ -207,48 +243,32 @@ enum sampleHold_enum
 };
 
 
-
-                             /* private */
-
+//////////////////////////////////////////////////////////////////
 
 typedef union {
    uint32_t i;
-   MSP430ADC12Settings_t s;
-} MSP430ADC12Settings_ut;
+   msp430adc12_channel_config_t s;
+} msp430adc12_channel_config_ut;
 
 
-inline MSP430ADC12Settings_t int2adcSettings(uint32_t i){
-  MSP430ADC12Settings_ut u;
+inline msp430adc12_channel_config_t int2adcSettings(uint32_t i){
+  msp430adc12_channel_config_ut u;
   u.i = i;
   return u.s;
 }
 
-enum
-{
-  ADC_IDLE = 0,
-  SINGLE_CHANNEL = 1,
-  REPEAT_SINGLE_CHANNEL = 2,
-  SEQUENCE_OF_CHANNELS = 4,
-  REPEAT_SEQUENCE_OF_CHANNELS = 8,
-  TIMER_USED = 16,
-  RESERVED = 32,
-  VREF_WAIT = 64,
-};
+// The unique string for allocating ADC resource interfaces
+#define MSP430ADC12_CLIENT "MSP430ADC12Client"
   
 /* Test for GCC bug (bitfield access) - only version 3.2.3 is known to be stable */
 #define GCC_VERSION (__GNUC__ * 100 + __GNUC_MINOR__ * 10 + __GNUC_PATCHLEVEL__)
 #if GCC_VERSION == 332
-#error "Your gcc version (3.3.2) contains a bug which results in false accessing \
-of bitfields in structs and makes MSP430ADC12M.nc fail ! Install version 3.2.3 instead."
+#error "The msp430-gcc version (3.3.2) contains a bug which results in false accessing \
+of bitfields in structs and makes MSP430ADC12M.nc fail ! Use version 3.2.3 instead."
 #elif GCC_VERSION != 323
-#warning "This version of gcc might contain a bug which results in false accessing \
-of bitfields in structs (MSP430ADC12M.nc would fail). Version 3.2.3 is known to be safe."
+#warning "This version of msp430-gcc might contain a bug which results in false accessing \
+of bitfields in structs (MSP430ADC12M.nc would fail). Use version 3.2.3 instead."
 #endif  
-
-#define ADC12CTL0_DEFAULT 0x0000
-#define ADC12CTL0_TIMER_TRIGGERED ((ADC12CTL0_DEFAULT | ADC12ON) & ~(MSC))
-#define ADC12CTL0_AUTO_TRIGGERED ((ADC12CTL0_DEFAULT | ADC12ON) | MSC)
-
 
 typedef struct 
 {
@@ -257,19 +277,5 @@ typedef struct
   sref: 3,                                     // reference voltage
   eos: 1;                                      // end of sequence flag
 } __attribute__ ((packed)) adc12memctl_t;
-
-typedef struct 
-{
-  unsigned int refVolt2_5: 1;
-  unsigned int gotRefVolt: 1;
-  unsigned int result_16bit: 1;
-  unsigned int clockSourceSHT: 2;
-  unsigned int clockSourceSAMPCON: 2;
-  unsigned int clockDivSAMPCON: 2;  
-  unsigned int clockDivSHT: 3;            
-  unsigned int sampleHoldTime: 4; 
-  adc12memctl_t memctl;
-} __attribute__ ((packed)) adc12settings_t;
-
 
 #endif
