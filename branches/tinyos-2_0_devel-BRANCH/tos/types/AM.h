023484e289461c3ca35b18e155e3aa9b144a9dab
scipio <scipio@285f258d-8064-b571-fbc6-9656dc998e90>
2005-06-06 17:16:01 +0000

    Updating ActiveMessages.
    git-svn-id: http://tinyos-main.googlecode.com/svn@405 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/types/AM.h
+++ branches/tinyos-2_0_devel-BRANCH/tos/types/AM.h
@@ -1,11 +1,11 @@
 typedef uint8_t am_id_t;
 typedef uint16_t am_addr_t;
 
-typedef struct AMHeader {
-  am_id_t type;
-} AMHeader;
+typedef nx_struct {
+  nx_uint8_t type;
+  nx_uint8_t data[0];
+} ActiveMsg;
 
 enum {
   AM_BROADCAST_ADDR = 0xffff,
-  AM_HEADER_SIZE = sizeof(AMHeader),
 };
