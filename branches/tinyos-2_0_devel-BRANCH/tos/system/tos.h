5df3ac41a1fe52ab0ec53a312e18c3bb5121b480
idgay <idgay@285f258d-8064-b571-fbc6-9656dc998e90>
2006-01-16 19:06:10 +0000

    PASS annotation
    git-svn-id: http://tinyos-main.googlecode.com/svn@1192 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/system/tos.h
+++ branches/tinyos-2_0_devel-BRANCH/tos/system/tos.h
@@ -22,6 +22,10 @@ enum { FALSE = 0, TRUE = 1 };
 typedef int8_t nx_bool __attribute__((nx_base(int8)));
 uint16_t TOS_LOCAL_ADDRESS = 1;
 
+/* This macro is used to mark pointers that represent ownership
+   transfer in interfaces. See TEP 3 for more discussion. */
+#define PASS
+
 struct @atmostonce { };
 struct @atleastonce { };
 struct @exactlyonce { };
