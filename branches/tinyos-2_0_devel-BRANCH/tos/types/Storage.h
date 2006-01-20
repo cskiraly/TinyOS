3aac95dcab5b199f028681539b33dc93dc3fdcdf
jwhui <jwhui@285f258d-8064-b571-fbc6-9656dc998e90>
2006-01-20 01:08:46 +0000

    Take out storage_error_t and use error_t instead.
    git-svn-id: http://tinyos-main.googlecode.com/svn@1238 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/types/Storage.h
+++ branches/tinyos-2_0_devel-BRANCH/tos/types/Storage.h
@@ -1,4 +1,4 @@
-// $Id: Storage.h,v 1.1.2.3 2006-01-18 00:21:04 idgay Exp $
+// $Id: Storage.h,v 1.1.2.4 2006-01-20 01:08:46 jwhui Exp $
 
 /*									tab:2
  * "Copyright (c) 2000-2005 The Regents of the University  of California.  
@@ -39,19 +39,10 @@
 
 #define UQ_STORAGE_VOLUME "storage.resource"
 
-enum {
-  STORAGE_OK,
-  STORAGE_FAIL,
-  STORAGE_INVALID_SIGNATURE,
-  STORAGE_INVALID_CRC
-};
-
-enum {
-  STORAGE_INVALID_ADDR = (uint32_t)0xffff,
-};
-
+typedef uint8_t storage_volume_t;
 typedef uint8_t volume_id_t;
-typedef uint8_t storage_result_t;
+typedef uint32_t storage_addr_t;
+typedef uint16_t storage_len_t;
 
 #include "Storage_chip.h"
 
