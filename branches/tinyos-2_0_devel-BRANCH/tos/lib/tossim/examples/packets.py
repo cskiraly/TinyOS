6d9272f84f147b4d8aa685f5b4465cbdcf0dd547
scipio <scipio@285f258d-8064-b571-fbc6-9656dc998e90>
2006-02-03 22:31:29 +0000

    New packet interface. Example of variable interface.
    git-svn-id: http://tinyos-main.googlecode.com/svn@1534 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/lib/tossim/examples/packets.py
+++ branches/tinyos-2_0_devel-BRANCH/tos/lib/tossim/examples/packets.py
@@ -1,32 +1,53 @@
-from TOSSIM import *
-import sys, os, string, types
-from struct import calcsize, pack, unpack
-from gc import *
+import TOSSIM
+import sys
 
-t = Tossim();
-t.init();
+execfile('RadioCountMsg.py')
 
-m = t.getNode(0);
-m.bootAtTime(500);
+t = TOSSIM.Tossim([])
+m = t.mac();
+r = t.radio();
+t.init()
 
-#t.addChannel("LedsC", sys.stdout);
-#t.addChannel("Packet", sys.stdout);
-#t.addChannel("RadioCountToLedsC", sys.stdout);
-#t.addChannel("AM", sys.stdout);
-i = 0
-d = pack('!H', i % 7);
+t.addChannel("RadioCountToLedsC", sys.stdout);
+t.addChannel("LedsC", sys.stdout);
+t.addChannel("Packet", sys.stdout);
+t.addChannel("AM", sys.stdout);
+t.addChannel("AMQueue", sys.stdout);
+#t.addChannel("Gain", sys.stdout);
+#t.addChannel("TossimPacketModelC", sys.stdout);
 
-while(t.time() <= 6000000000):
+print (dir(TOSSIM.Tossim))
+
+for i in range(0, 2):
+  m = t.getNode(i);
+  m.bootAtTime(500000003 * i + 1);
+  print "Mote " + str(i) + " set to boot at " + str(500000003 * i + 1);
+  r.setNoise(i, -77.0, 3.0);
+  for j in range (0, 2):
+    if (j != i):
+      r.add(i, j, -50.0);
+ 
+#t.addChannel("BlinkC", sys.stdout);
+#t.addChannel("HplAtm128CompareC", sys.stdout);
+#t.addChannel("Atm128AlarmC", sys.stdout);
+#t.addChannel("HplCounter0C", sys.stdout);
+
+for i in range(0, 60):
   t.runNextEvent();
-  p = t.newPacket();
-  i = i + 1;
-  p.setDestination(0);
-  p.setType(6);
-  p.deliver(0, t.time() + 10000);
-  p.setData(d);
-  collect();
-  
-print "Messages complete."
 
-while (1):
+
+msg = RadioCountMsg()
+msg.set_counter(7);
+pkt = t.newPacket();
+pkt.setData(msg.data)
+pkt.setType(msg.get_amType())
+pkt.setDestination(0)
+
+print "Delivering " + msg.__str__() + " to 0 at " + str(t.time() + 3);
+pkt.deliver(0, t.time() + 3)
+
+
+for i in range(0, 20):
   t.runNextEvent();
+
+
