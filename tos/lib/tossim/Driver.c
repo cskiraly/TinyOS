1b814b64a9f9a55b66448e8e17964fe4027fa3d2
scipio <scipio@285f258d-8064-b571-fbc6-9656dc998e90>
2006-02-03 22:26:21 +0000

    New Driver, skeletal README.
    git-svn-id: http://tinyos-main.googlecode.com/svn/branches/tinyos-2_0_devel-BRANCH@1533 285f258d-8064-b571-fbc6-9656dc998e90

--- tos/lib/tossim/Driver.c
+++ tos/lib/tossim/Driver.c
@@ -1,47 +1,28 @@
-/*
- * "Copyright (c) 2005 Stanford University. All rights reserved.
- *
- * Permission to use, copy, modify, and distribute this software and
- * its documentation for any purpose, without fee, and without written
- * agreement is hereby granted, provided that the above copyright
- * notice, the following two paragraphs and the author appear in all
- * copies of this software.
- * 
- * IN NO EVENT SHALL STANFORD UNIVERSITY BE LIABLE TO ANY PARTY FOR
- * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
- * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN
- * IF STANFORD UNIVERSITY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
- * DAMAGE.
- * 
- * STANFORD UNIVERSITY SPECIFICALLY DISCLAIMS ANY WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE
- * PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND STANFORD UNIVERSITY
- * HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
- * ENHANCEMENTS, OR MODIFICATIONS."
- */
-
-/**
- * Simple example C++ driver for a TOSSIM simulation.
- *
- * @author Philip Levis
- * @date   Nov 22 2005
- */
-
-// $Id: Driver.c,v 1.1.2.1 2005-11-22 23:29:13 scipio Exp $
-
 #include <tossim.h>
 
 int main() {
-  Tossim* t = new Tossim();
+  Tossim* t = new Tossim(NULL);
+  t->init();
+  //t->addChannel("Scheduler", fdopen(1, "w"));
+  //t->addChannel("TossimPacketModelC", fdopen(1, "w"));
+  t->addChannel("LedsC", fdopen(1, "w"));
+  t->addChannel("AM", fdopen(1, "w"));
 
-  // Boot 20 nodes at time 1, 501, 1001 ... 9501
-  for (int i = 0; i < 20; i++) {
+  Radio* r = t->radio();
+    
+  for (int i = 0; i < 2; i++) {
+    printf("Mote %i at %i\n", i, 15000000 * i + 1);
     Mote* m = t->getNode(i);
-    m->bootAtTime(500 * i + 1);
+    m->bootAtTime(15000000 * i + 1);
+    r->setNoise(i, -77.0, 3);
+    for (int j = 0; j < 2; j++) {
+      if (i != j) {
+        r->add(i, j, -50.0);
+      }
+    }
   }
 
-  while (t->runNextEvent()) {
-
+  for (int i = 0; i < 60; i++) {
+    t->runNextEvent();
   }
 }
