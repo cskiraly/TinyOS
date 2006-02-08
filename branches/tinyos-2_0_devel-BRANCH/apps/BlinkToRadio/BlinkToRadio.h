8caf22a7056e2a231e13420aa77c16d8cc932e28
prabal <prabal@285f258d-8064-b571-fbc6-9656dc998e90>
2006-02-08 08:50:07 +0000

    Application for Tutorial Lesson 5
    git-svn-id: http://tinyos-main.googlecode.com/svn@1586 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ branches/tinyos-2_0_devel-BRANCH/apps/BlinkToRadio/BlinkToRadio.h
@@ -0,0 +1,16 @@
+// $Id: BlinkToRadio.h,v 1.1.2.1 2006-02-08 08:50:07 prabal Exp $
+
+#ifndef BLINKTORADIO_H
+#define BLINKTORADIO_H
+
+enum {
+  AM_BLINKTORADIO = 6,
+  TIMER_PERIOD_MILLI = 250
+};
+
+typedef nx_struct BlinkToRadioMsg {
+  nx_uint16_t nodeid;
+  nx_uint16_t counter;
+} BlinkToRadioMsg;
+
+#endif
