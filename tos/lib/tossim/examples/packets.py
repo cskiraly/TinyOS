daf10d47f66bdcb8c06da221298e9c041edd4201
scipio <scipio@285f258d-8064-b571-fbc6-9656dc998e90>
2006-03-07 02:19:01 +0000

    Updated for TOSSIM tutorial.
    git-svn-id: http://tinyos-main.googlecode.com/svn/branches/tinyos-2_0_devel-BRANCH@1682 285f258d-8064-b571-fbc6-9656dc998e90

--- tos/lib/tossim/examples/packets.py
+++ tos/lib/tossim/examples/packets.py
@@ -1,40 +1,33 @@
-import TOSSIM
-import sys
+# This file is an example Python script from the TOSSIM tutorial.
+# It is intended to be used with the RadioCountToLeds application.
 
-execfile('RadioCountMsg.py')
+import sys
+from TOSSIM import *
+from RadioCountMsg import *
 
 t = TOSSIM.Tossim([])
 m = t.mac();
 r = t.radio();
-t.init()
 
 t.addChannel("RadioCountToLedsC", sys.stdout);
 t.addChannel("LedsC", sys.stdout);
-t.addChannel("Packet", sys.stdout);
-t.addChannel("AM", sys.stdout);
-t.addChannel("AMQueue", sys.stdout);
-#t.addChannel("Gain", sys.stdout);
-#t.addChannel("TossimPacketModelC", sys.stdout);
-
-print (dir(TOSSIM.Tossim))
 
 for i in range(0, 2):
   m = t.getNode(i);
   m.bootAtTime(500000003 * i + 1);
-  print "Mote " + str(i) + " set to boot at " + str(500000003 * i + 1);
-  r.setNoise(i, -77.0, 3.0);
-  for j in range (0, 2):
-    if (j != i):
-      r.add(i, j, -50.0);
- 
-#t.addChannel("BlinkC", sys.stdout);
-#t.addChannel("HplAtm128CompareC", sys.stdout);
-#t.addChannel("Atm128AlarmC", sys.stdout);
-#t.addChannel("HplCounter0C", sys.stdout);
+
+f = open("topo.txt", "r")
+lines = f.readlines()
+for line in lines:
+  s = line.split()
+  if (len(s) > 0):
+    if (s[0] == "gain"):
+      r.add(int(s[1]), int(s[2]), float(s[3]))
+    elif (s[0] == "noise"):
+      r.setNoise(int(s[1]), float(s[2]), float(s[3]))
 
 for i in range(0, 60):
   t.runNextEvent();
-
 
 msg = RadioCountMsg()
 msg.set_counter(7);
@@ -49,5 +42,4 @@ pkt.deliver(0, t.time() + 3)
 
 for i in range(0, 20):
   t.runNextEvent();
-
 
