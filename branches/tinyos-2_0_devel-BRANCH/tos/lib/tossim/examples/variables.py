08edba27ba988cbf9dcc23ccc8d6dfb89e62dc84
scipio <scipio@285f258d-8064-b571-fbc6-9656dc998e90>
2006-03-07 02:19:01 +0000

    Updated for TOSSIM tutorial.
    git-svn-id: http://tinyos-main.googlecode.com/svn@1682 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/lib/tossim/examples/variables.py
+++ branches/tinyos-2_0_devel-BRANCH/tos/lib/tossim/examples/variables.py
@@ -1,24 +1,12 @@
+# This is an example script from the TOSSIM tutorials.
+# It can be used with any TinyOS application.
+
 from tinyos.tossim.TossimApp import *
 from TOSSIM import *
 
-n = NescApp("Blink")
-print n
-
-print n.variables
-#print n.enums
-#print n.messages
-#print n.types
-
-print(n.variables)
-print(n.variables.variables())
-#print dir(n.variables.variables())
-
-vars = n.variables.variables()
-
-t = Tossim(vars)
+n = NescApp()
+t = Tossim(n.variables.variables())
 m = t.getNode(0)
-
-#t.addChannel("SimMoteP", sys.stdout);
 
 for i in range(0, 1):
   m = t.getNode(i);
@@ -28,16 +16,14 @@ for i in range(0, 1):
 for i in range(0, 500):
   t.runNextEvent();
 
-v = m.getVariable("TestSerialC.floatTest")
-v2 = m.getVariable("TestSerialC.arrayTest");
+v = m.getVariable("SimMoteP.startTime")
+v2 = m.getVariable("SimSchedulerBasicP.m_head");
 
-#print "Variables: ", v, " ", v2
-
-print "v1: <", v.getData(), ">\nv2: <", v2.getData(), ">"
+print "start time: <", v.getData(), ">\nnext task: <", v2.getData(), ">"
 
 for i in range(0, 500):
   t.runNextEvent();
 
-print "v1: <", v.getData(), ">\nv2: <", v2.getData(), ">"
+print "start time: <", v.getData(), ">\nnext task: <", v2.getData(), ">"
 
 
