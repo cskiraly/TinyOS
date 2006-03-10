03d0f4a99b90549258ede118ac5f9b2c488f5c7d
scipio <scipio@285f258d-8064-b571-fbc6-9656dc998e90>
2006-03-10 18:13:27 +0000

    Removed constant that prevented simulating more than 100 nodes. Constant was a hold-over from really early code where I was testing out maintaining register files.
    git-svn-id: http://tinyos-main.googlecode.com/svn@1692 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/chips/atm128/sim/atm128hardware.h
+++ branches/tinyos-2_0_devel-BRANCH/tos/chips/atm128/sim/atm128hardware.h
@@ -33,7 +33,7 @@
  *  @author Jason Hill, Philip Levis, Nelson Lee, David Gay
  *  @author Martin Turon <mturon@xbow.com>
  *
- *  $Id: atm128hardware.h,v 1.1.2.3 2006-01-27 23:13:22 idgay Exp $
+ *  $Id: atm128hardware.h,v 1.1.2.4 2006-03-10 18:13:27 scipio Exp $
  */
 
 #ifndef _H_atmega128hardware_H
@@ -42,7 +42,7 @@
 #include <atm128_sim.h>
 #include <sim_tossim.h>
 
-uint8_t atm128RegFile[100][0xa0];
+uint8_t atm128RegFile[TOSSIM_MAX_NODES][0xa0];
 
 #define REG_ACCESS(x) atm128RegFile[sim_node()][x]
 
