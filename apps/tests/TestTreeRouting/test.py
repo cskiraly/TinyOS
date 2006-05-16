a13b6d63ef59bd0dceda3135852878c4565e31f2
rfonseca76 <rfonseca76@285f258d-8064-b571-fbc6-9656dc998e90>
2006-05-16 22:47:21 +0000

    Simple test with two roots
    git-svn-id: http://tinyos-main.googlecode.com/svn/branches/tinyos-2_0_devel-BRANCH@1950 285f258d-8064-b571-fbc6-9656dc998e90

--- apps/tests/TestTreeRouting/test.py
+++ apps/tests/TestTreeRouting/test.py
@@ -14,7 +14,7 @@ t.init()
 t.addChannel("AM", sys.stdout);
 t.addChannel("TreeRouting", sys.stdout);
 t.addChannel("TreeRoutingCtl", sys.stdout);
-#t.addChannel("LI", sys.stdout);
+t.addChannel("LI", sys.stdout);
 #t.addChannel("Gain", sys.stdout);
 #t.addChannel("TossimPacketModelC", sys.stdout);
 
@@ -30,10 +30,15 @@ for line in lines:
     elif (s[0] == "noise"):
       r.setNoise(int(s[1]), float(s[2]), float(s[3]))
 
-for i in range(0, 2):
+for i in range(0, 225):
   m = t.getNode(i);
-  m.bootAtTime((t.ticksPerSecond() / 50) * i + 43);
+  if (i == 173):
+    m.bootAtTime(t.ticksPerSecond() * 100) 
+  else:
+    m.bootAtTime((t.ticksPerSecond() / 50) * i + 43);
  
 while (t.time() / t.ticksPerSecond() < 1600):
+  if (t.time() == t.ticksPerSecond() * 100):
+    print "---------"
   t.runNextEvent()
   
