34765c8abdc0cc2554a992843469e096425bd50c
gnawali <gnawali@285f258d-8064-b571-fbc6-9656dc998e90>
2006-06-09 02:23:23 +0000

    wiring change to add new am type sender/receiver for link estimator
    git-svn-id: http://tinyos-main.googlecode.com/svn/branches/tinyos-2_0_devel-BRANCH@2104 285f258d-8064-b571-fbc6-9656dc998e90

--- tos/lib/net/collection/Collection.h
+++ tos/lib/net/collection/Collection.h
@@ -1,4 +1,4 @@
-/* $Id: Collection.h,v 1.1.2.5 2006-05-23 20:55:55 kasj78 Exp $ */
+/* $Id: Collection.h,v 1.1.2.6 2006-06-09 02:23:23 gnawali Exp $ */
 /*
  * "Copyright (c) 2005 The Regents of the University  of California.  
  * All rights reserved.
@@ -23,7 +23,7 @@
 
 /*
  *  @author Rodrigo Fonseca
- *  @date   $Date: 2006-05-23 20:55:55 $
+ *  @date   $Date: 2006-06-09 02:23:23 $
  */
 #ifndef COLLECTION_H
 #define COLLECTION_H
@@ -31,6 +31,7 @@
 enum {
     AM_COLLECTION_DATA = 20,
     AM_COLLECTION_CONTROL = 21,
+    AM_LINKEST = 0,
 };
 
 typedef uint8_t collection_id_t;
