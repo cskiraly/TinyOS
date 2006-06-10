e94c3b04c70faebe59fa7d36d016d8a166a73bf2
scipio <scipio@285f258d-8064-b571-fbc6-9656dc998e90>
2006-06-10 21:19:50 +0000

    Send at 1Hz.
    git-svn-id: http://tinyos-main.googlecode.com/svn/branches/tinyos-2_0_devel-BRANCH@2125 285f258d-8064-b571-fbc6-9656dc998e90

--- apps/tests/TestNetwork/test.py
+++ apps/tests/TestNetwork/test.py
@@ -8,18 +8,18 @@ import sys
 t = Tossim([])
 r = t.radio()
 
-f = open("kaisen-topo.txt", "r")
+f = open("topo-50.txt", "r")
 lines = f.readlines()
 for line in lines:
   s = line.split()
   if (len(s) > 0):
-    if s[0] == "gain":
+    if s[0] == "gain" and int(s[1]) % 5 == 0 and int(s[2]) % 5 == 0:
       r.add(int(s[1]), int(s[2]), float(s[3]))
     elif s[0] == "noise":
       r.setNoise(int(s[1]), float(s[2]), float(s[3]))
 
-for i in range(0, 6):
-  m = t.getNode(i);
+for i in range(0, 10):
+  m = t.getNode(i * 5);
   time = randint(t.ticksPerSecond(), 10 * t.ticksPerSecond())
   m.bootAtTime(time)
   print "Booting ", i, " at time ", time
@@ -30,9 +30,12 @@ print "Starting simulation."
 #t.addChannel("TreeRouting", sys.stdout)
 #t.addChannel("TestNetworkC", sys.stdout)
 #t.addChannel("TestNetworkC", sys.stdout)
-t.addChannel("Traffic", sys.stdout)
-#t.addChannel("LI", sys.stdout)
-#t.addChannel("LITest", sys.stdout)
+t.addChannel("Route", sys.stdout)
+t.addChannel("PointerBug", sys.stdout)
+t.addChannel("QueueC", sys.stdout)
+#t.addChannel("Gain", sys.stdout)
+t.addChannel("Forwarder", sys.stdout)
+t.addChannel("Acks", sys.stdout)
 
 while (t.time() < 300 * t.ticksPerSecond()):
   t.runNextEvent()
