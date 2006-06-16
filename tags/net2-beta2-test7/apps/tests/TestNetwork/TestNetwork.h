a96eb43c21a8bcf7d2f456fc38adfbb8fcad9214
(no author) <(no author)@285f258d-8064-b571-fbc6-9656dc998e90>
2006-06-16 13:48:56 +0000

    This commit was manufactured by cvs2svn to create tag 'net2-beta2-test7'.
    git-svn-id: http://tinyos-main.googlecode.com/svn@2210 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ tags/net2-beta2-test7/apps/tests/TestNetwork/TestNetwork.h
@@ -0,0 +1,16 @@
+#ifndef TEST_NETWORK_H
+#define TEST_NETWORK_H
+
+#include <AM.h>
+#include "TestNetworkC.h"
+
+typedef nx_struct TestNetworkMsg {
+  nx_am_addr_t source;
+  nx_uint16_t seqno;
+  nx_am_addr_t parent;
+  nx_uint16_t metric;
+  nx_uint16_t data;
+  nx_uint8_t hopcount;
+} TestNetworkMsg;
+
+#endif
