e3ef056bdcc2a9482ece9cfcb22cf4eedeafe163
(no author) <(no author)@285f258d-8064-b571-fbc6-9656dc998e90>
2006-06-21 02:16:54 +0000

    This commit was manufactured by cvs2svn to create tag 'net2-beta2-test11'.
    git-svn-id: http://tinyos-main.googlecode.com/svn@2258 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ tags/net2-beta2-test11/tos/lib/net/collection/ForwardingEngine.h
@@ -0,0 +1,69 @@
+#ifndef FORWARDING_ENGINE_H
+#define FORWARDING_ENGINE_H
+
+#include <AM.h>
+#include <message.h>
+
+#if PLATFORM_MICA2
+
+// 512-1023 ms
+#define SENDDONE_FAIL_WINDOW 0x01ff
+#define SENDDONE_FAIL_OFFSET 512
+
+// 128-255 ms.
+#define SENDDONE_NOACK_WINDOW 0x007f
+#define SENDDONE_NOACK_OFFSET 128
+
+// 128-255 ms.
+#define SENDDONE_OK_WINDOW 0x007f
+#define SENDDONE_OK_OFFSET 128
+
+#elif PLATFORM_MICAZ
+
+// 512-1023 ms
+#define SENDDONE_FAIL_WINDOW 0x01ff
+#define SENDDONE_FAIL_OFFSET 512
+
+// 128-255 ms.
+#define SENDDONE_NOACK_WINDOW 0x007f
+#define SENDDONE_NOACK_OFFSET 128
+
+// 128-255 ms.
+#define SENDDONE_OK_WINDOW 0x007f
+#define SENDDONE_OK_OFFSET 128
+
+#elif PLATFORM_TELOS
+
+// 512-1023 ms
+#define SENDDONE_FAIL_WINDOW 0x01ff
+#define SENDDONE_FAIL_OFFSET 512
+
+// 64-128 ms
+#define SENDDONE_NOACK_WINDOW 0x003f
+#define SENDDONE_NOACK_OFFSET 64
+
+// 32-64 ms
+#define SENDDONE_OK_WINDOW 0x001f
+#define SENDDONE_OK_OFFSET 32
+
+#else
+#error "forwarding engine todo"
+#endif
+
+enum {
+  MAX_RETRIES = 4
+};
+
+typedef nx_struct {
+  nx_uint8_t control;
+  nx_am_addr_t origin;
+  nx_uint8_t collectid;
+} network_header_t;
+
+typedef struct {
+  message_t *msg;
+  uint8_t client;
+  uint8_t retries;
+} fe_queue_entry_t;
+
+#endif
