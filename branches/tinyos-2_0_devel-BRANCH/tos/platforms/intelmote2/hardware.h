10b221642c2142f8c41a411918f8f76047ca648f
philipb <philipb@285f258d-8064-b571-fbc6-9656dc998e90>
2006-06-22 18:35:47 +0000

    Added pin and altfns for IM2 I2C
    git-svn-id: http://tinyos-main.googlecode.com/svn@2285 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/tos/platforms/intelmote2/hardware.h
+++ branches/tinyos-2_0_devel-BRANCH/tos/platforms/intelmote2/hardware.h
@@ -223,6 +223,11 @@ const uint8_t TOSH_IRP_TABLE[] = { 0xFF, // PPID  0 SSP_3 Service Req
 #define STUART_TXD (47) 
 #define STUART_TXD_ALTFN (1)
 
+#define I2C_SCL (117)
+#define I2C_SCL_ALTFN (1)
+#define I2C_SDA (118)
+#define I2D_SDA_ALTFN (1)
+
 
 #if 0
 TOSH_ASSIGN_PIN(CC_VREN,A,CC_VREN_PIN); 
