f8e1875b3af8ebe13dd812eef7c8f66c2e2b6711
scipio <scipio@285f258d-8064-b571-fbc6-9656dc998e90>
2006-10-30 00:52:32 +0000

    Include routing info and image.
    git-svn-id: http://tinyos-main.googlecode.com/svn@2766 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/apps/MViz/MViz.h
+++ branches/tinyos-2_0_devel-BRANCH/apps/MViz/MViz.h
@@ -17,18 +17,23 @@
 #ifndef MVIZ_H
 #define MVIZ_H
 
+#include "AM.h"
+
 enum {
   /* Default sampling period. */
   DEFAULT_INTERVAL = 128,
   AM_MVIZ_MSG = 0x93
 };
 
-typedef nx_struct mviz_msg{
+typedef nx_struct mviz_msg {
   nx_uint16_t version; /* Version of the interval. */
   nx_uint16_t interval; /* Samping period. */
   nx_uint16_t origin; /* Mote id of sending mote. */
   nx_uint16_t count; /* The readings are samples count * NREADINGS onwards */
   nx_uint16_t reading;
+  nx_uint16_t etx;
+  nx_uint16_t link_route_value;
+  nx_am_addr_t link_route_addr;
 } mviz_msg_t;
 
 #endif
