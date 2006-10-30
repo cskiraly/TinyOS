b1963df5bd4b244ab75fba86dd5ec5124aae89fc
henridf <henridf@285f258d-8064-b571-fbc6-9656dc998e90>
2006-10-30 14:56:57 +0000

    support tinynode
    git-svn-id: http://tinyos-main.googlecode.com/svn@2777 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/tinyos-2_0_devel-BRANCH/support/sdk/java/net/tinyos/packet/BaudRate.java
+++ branches/tinyos-2_0_devel-BRANCH/support/sdk/java/net/tinyos/packet/BaudRate.java
@@ -15,6 +15,7 @@ class BaudRate {
 	Platform.add(Platform.x, "mica2dot",   19200);
 	Platform.add(Platform.x, "telos",      115200);
 	Platform.add(Platform.x, "telosb",     115200);
+	Platform.add(Platform.x, "tinynode",     115200);
 	Platform.add(Platform.x, "tmote",      115200);
 	Platform.add(Platform.x, "micaz",      57600);
 	Platform.add(Platform.x, "eyesIFX",       57600);
