93f937ef38a718f40be0fb40719e3a15d72b44b4
(no author) <(no author)@285f258d-8064-b571-fbc6-9656dc998e90>
2008-06-24 14:46:19 +0000

    This commit was manufactured by cvs2svn to create tag 'rc_2_tinyos_2_1_0'.
    git-svn-id: http://tinyos-main.googlecode.com/svn@4144 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ tags/rc_2_tinyos_2_1_0/tos/lib/safe/include/annots_stage1.h
@@ -0,0 +1,33 @@
+/* Keep TinyOS happy with pre 1.3.0 nesC's */
+
+#ifndef ANNOTS_STAGE1_INCLUDED
+#define ANNOTS_STAGE1_INCLUDED
+
+// define away two obsolete annotations
+#define BOUND(x,y)
+#define SINGLE
+
+#if NESC < 130 
+
+#define __DEPUTY_UNUSED__                      __attribute__((unused))
+
+#define NONNULL                                
+#define BND(x,y)                             
+#define BND_NOK(x,y)                             
+#define COUNT(x)                               
+#define COUNT_NOK(x)                               
+#define ONE 
+#define ONE_NOK
+#define DMEMSET(x,y,z)
+#define DMEMCPY(x,y,z)
+#define TRUSTEDBLOCK
+
+#define TCAST(__type,__expr)                   ((__type)(__expr))                
+
+#ifdef SAFE_TINYOS
+#warning Safe TinyOS requires nesC >= 1.3.0
+#endif
+
+#endif // NESC version check
+
+#endif
