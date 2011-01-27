ba15eb8ba90d26e84f339f25f6e8ae62f2f14233
thomas.schmid <thomas.schmid@285f258d-8064-b571-fbc6-9656dc998e90>
2011-01-27 15:49:17 +0000

    Cleanup of PMC for Sam3u and Sam3s.
    git-svn-id: http://tinyos-main.googlecode.com/svn@5354 285f258d-8064-b571-fbc6-9656dc998e90

--- branches/cortex-devel/tos/chips/cortex/m3/sam3/pmc/pmchardware.h
+++ branches/cortex-devel/tos/chips/cortex/m3/sam3/pmc/pmchardware.h
@@ -89,158 +89,6 @@ typedef union
 } pmc_scsr_t;
 
 /**
- * PMC Peripheral Clock Enable Register, AT91 ARM Cortex-M3 based Microcontrollers
- * SAM3U Series, Preliminary, p. 485
- * 0: no effect
- * 1: enable corresponding peripheral clock
- */
-typedef union
-{
-    uint32_t flat;
-    struct
-    {
-        uint8_t reserved0 : 2;
-        uint8_t rtc       : 1; // not used after datasheet
-        uint8_t rtt       : 1; // not used after datasheet
-        uint8_t wdg       : 1; // not used after datasheet
-        uint8_t pmc       : 1; // not used after datasheet
-        uint8_t efc0      : 1; // not used after datasheet
-        uint8_t efc1      : 1; // not used after datasheet
-        uint8_t dbgu      : 1;
-        uint8_t hsmc4     : 1;
-        uint8_t pioa      : 1;
-        uint8_t piob      : 1;
-        uint8_t pioc      : 1;
-        uint8_t us0       : 1;
-        uint8_t us1       : 1;
-        uint8_t us2       : 1;
-        uint8_t us3       : 1;
-        uint8_t mci0      : 1;
-        uint8_t twi0      : 1;
-        uint8_t twi1      : 1;
-        uint8_t spi0      : 1;
-        uint8_t ssc0      : 1;
-        uint8_t tc0       : 1;
-        uint8_t tc1       : 1;
-        uint8_t tc2       : 1;
-        uint8_t pwmc      : 1;
-        uint8_t adc12b    : 1;
-        uint8_t adc       : 1;
-        uint8_t hdma      : 1;
-        uint8_t udphs     : 1;
-        uint8_t pid30     : 1; // not used on sam3u
-        uint8_t pid31     : 1; // not use don sam3u
-    } __attribute__((__packed__)) bits;
-} pmc_pcer_t;
-
-/**
- * PMC Peripheral Clock Disable Register, AT91 ARM Cortex-M3 based Microcontrollers
- * SAM3U Series, Preliminary, p. 486
- * 0: no effect
- * 1: disable corresponding peripheral clock
- */
-typedef union
-{
-    uint32_t flat;
-    struct
-    {
-        uint8_t reserved0 : 2;
-        uint8_t rtc       : 1; // not used after datasheet
-        uint8_t rtt       : 1; // not used after datasheet
-        uint8_t wdg       : 1; // not used after datasheet
-        uint8_t pmc       : 1; // not used after datasheet
-        uint8_t efc0      : 1; // not used after datasheet
-        uint8_t efc1      : 1; // not used after datasheet
-        uint8_t dbgu      : 1;
-        uint8_t hsmc4     : 1;
-        uint8_t pioa      : 1;
-        uint8_t piob      : 1;
-        uint8_t pioc      : 1;
-        uint8_t us0       : 1;
-        uint8_t us1       : 1;
-        uint8_t us2       : 1;
-        uint8_t us3       : 1;
-        uint8_t mci0      : 1;
-        uint8_t twi0      : 1;
-        uint8_t twi1      : 1;
-        uint8_t spi0      : 1;
-        uint8_t ssc0      : 1;
-        uint8_t tc0       : 1;
-        uint8_t tc1       : 1;
-        uint8_t tc2       : 1;
-        uint8_t pwmc      : 1;
-        uint8_t adc12b    : 1;
-        uint8_t adc       : 1;
-        uint8_t hdma      : 1;
-        uint8_t udphs     : 1;
-        uint8_t pid30     : 1; // not used on sam3u
-        uint8_t pid31     : 1; // not use don sam3u
-    } __attribute__((__packed__)) bits;
-} pmc_pcdr_t;
-
-/**
- * PMC Peripheral Clock Status Register, AT91 ARM Cortex-M3 based Microcontrollers
- * SAM3U Series, Preliminary, p. 487
- * 0: peripheral clock disabled
- * 1: peripheral clock enabled
- */
-typedef union
-{
-    uint32_t flat;
-    struct
-    {
-        uint8_t reserved0 : 2;
-        uint8_t rtc       : 1; // not used after datasheet
-        uint8_t rtt       : 1; // not used after datasheet
-        uint8_t wdg       : 1; // not used after datasheet
-        uint8_t pmc       : 1; // not used after datasheet
-        uint8_t efc0      : 1; // not used after datasheet
-        uint8_t efc1      : 1; // not used after datasheet
-        uint8_t dbgu      : 1;
-        uint8_t hsmc4     : 1;
-        uint8_t pioa      : 1;
-        uint8_t piob      : 1;
-        uint8_t pioc      : 1;
-        uint8_t us0       : 1;
-        uint8_t us1       : 1;
-        uint8_t us2       : 1;
-        uint8_t us3       : 1;
-        uint8_t mci0      : 1;
-        uint8_t twi0      : 1;
-        uint8_t twi1      : 1;
-        uint8_t spi0      : 1;
-        uint8_t ssc0      : 1;
-        uint8_t tc0       : 1;
-        uint8_t tc1       : 1;
-        uint8_t tc2       : 1;
-        uint8_t pwmc      : 1;
-        uint8_t adc12b    : 1;
-        uint8_t adc       : 1;
-        uint8_t hdma      : 1;
-        uint8_t udphs     : 1;
-        uint8_t pid30     : 1; // not used on sam3u
-        uint8_t pid31     : 1; // not use don sam3u
-    } __attribute__((__packed__)) bits;
-} pmc_pcsr_t;
-
-/**
- * PMC UTMI Clock Configuration Register, AT91 ARM Cortex-M3 based Microcontrollers
- * SAM3U Series, Preliminary, p. 488
- */
-typedef union
-{
-    uint32_t flat;
-    struct
-    {
-        uint16_t reserved0: 16;
-        uint8_t upllen    :  1; // UTMI PLL enable
-        uint8_t reserved1 :  3;
-        uint8_t upllcount :  4; // UTMI PLL Start-up Time (in number of slow clock cycles times 8
-        uint8_t reserved2 :  8; 
-    } __attribute__((__packed__)) bits;
-} pmc_uckr_t;
-
-/**
  * PMC Clock Generator Main Oscillator Register, AT91 ARM Cortex-M3 based Microcontrollers
  * SAM3U Series, Preliminary, p. 489
  *
@@ -564,12 +412,6 @@ typedef union
     } __attribute__((__packed__)) bits;
 } pmc_focr_t;
 
-typedef struct
-{
-    volatile pmc_pcer_t pcer; // Peripheral Clock Enable Register
-    volatile pmc_pcdr_t pcdr; // Peripheral Clock Disable Register
-    volatile pmc_pcsr_t pcsr; // Peripheral Clock Status Register
-} pmc_pc_t;
 
 /**
  * PMC Write Protect Mode Register.
@@ -582,7 +424,7 @@ typedef union
         uint32_t wpen       :  1; // Write Protect Enable
         uint32_t reserved0  :  7;
         uint32_t wpkey      : 24; // Write Protect Key
-    } __ attribute__((__packed__)) bits;
+    } __attribute__((__packed__)) bits;
 } pmc_wpmr_t;
 
 #define PMC_WPMR_WPKEY 0x504D43
@@ -599,7 +441,7 @@ typedef union
         uint32_t reserved0  :  7;
         uint32_t wpvsrc     : 16; // Write Protect Violation Source
         uint32_t reserved1  :  8;
-    } __ attribute__((__packed__)) bits;
+    } __attribute__((__packed__)) bits;
 } pmc_wpsr_t;
 
 #endif // PMCHARDWARE_H
--- branches/cortex-devel/tos/chips/cortex/m3/sam3/u/pmc/sam3upmchardware.h
+++ branches/cortex-devel/tos/chips/cortex/m3/sam3/u/pmc/sam3upmchardware.h
@@ -40,6 +40,166 @@
 
 #include "pmchardware.h"
 
+
+/**
+ * PMC Peripheral Clock Enable Register, AT91 ARM Cortex-M3 based Microcontrollers
+ * SAM3U Series, Preliminary, p. 485
+ * 0: no effect
+ * 1: enable corresponding peripheral clock
+ */
+typedef union
+{
+    uint32_t flat;
+    struct
+    {
+        uint8_t reserved0 : 2;
+        uint8_t rtc       : 1; // not used after datasheet
+        uint8_t rtt       : 1; // not used after datasheet
+        uint8_t wdg       : 1; // not used after datasheet
+        uint8_t pmc       : 1; // not used after datasheet
+        uint8_t efc0      : 1; // not used after datasheet
+        uint8_t efc1      : 1; // not used after datasheet
+        uint8_t dbgu      : 1;
+        uint8_t hsmc4     : 1;
+        uint8_t pioa      : 1;
+        uint8_t piob      : 1;
+        uint8_t pioc      : 1;
+        uint8_t us0       : 1;
+        uint8_t us1       : 1;
+        uint8_t us2       : 1;
+        uint8_t us3       : 1;
+        uint8_t mci0      : 1;
+        uint8_t twi0      : 1;
+        uint8_t twi1      : 1;
+        uint8_t spi0      : 1;
+        uint8_t ssc0      : 1;
+        uint8_t tc0       : 1;
+        uint8_t tc1       : 1;
+        uint8_t tc2       : 1;
+        uint8_t pwmc      : 1;
+        uint8_t adc12b    : 1;
+        uint8_t adc       : 1;
+        uint8_t hdma      : 1;
+        uint8_t udphs     : 1;
+        uint8_t pid30     : 1; // not used on sam3u
+        uint8_t pid31     : 1; // not use don sam3u
+    } __attribute__((__packed__)) bits;
+} pmc_pcer_t;
+
+/**
+ * PMC Peripheral Clock Disable Register, AT91 ARM Cortex-M3 based Microcontrollers
+ * SAM3U Series, Preliminary, p. 486
+ * 0: no effect
+ * 1: disable corresponding peripheral clock
+ */
+typedef union
+{
+    uint32_t flat;
+    struct
+    {
+        uint8_t reserved0 : 2;
+        uint8_t rtc       : 1; // not used after datasheet
+        uint8_t rtt       : 1; // not used after datasheet
+        uint8_t wdg       : 1; // not used after datasheet
+        uint8_t pmc       : 1; // not used after datasheet
+        uint8_t efc0      : 1; // not used after datasheet
+        uint8_t efc1      : 1; // not used after datasheet
+        uint8_t dbgu      : 1;
+        uint8_t hsmc4     : 1;
+        uint8_t pioa      : 1;
+        uint8_t piob      : 1;
+        uint8_t pioc      : 1;
+        uint8_t us0       : 1;
+        uint8_t us1       : 1;
+        uint8_t us2       : 1;
+        uint8_t us3       : 1;
+        uint8_t mci0      : 1;
+        uint8_t twi0      : 1;
+        uint8_t twi1      : 1;
+        uint8_t spi0      : 1;
+        uint8_t ssc0      : 1;
+        uint8_t tc0       : 1;
+        uint8_t tc1       : 1;
+        uint8_t tc2       : 1;
+        uint8_t pwmc      : 1;
+        uint8_t adc12b    : 1;
+        uint8_t adc       : 1;
+        uint8_t hdma      : 1;
+        uint8_t udphs     : 1;
+        uint8_t pid30     : 1; // not used on sam3u
+        uint8_t pid31     : 1; // not use don sam3u
+    } __attribute__((__packed__)) bits;
+} pmc_pcdr_t;
+
+/**
+ * PMC Peripheral Clock Status Register, AT91 ARM Cortex-M3 based Microcontrollers
+ * SAM3U Series, Preliminary, p. 487
+ * 0: peripheral clock disabled
+ * 1: peripheral clock enabled
+ */
+typedef union
+{
+    uint32_t flat;
+    struct
+    {
+        uint8_t reserved0 : 2;
+        uint8_t rtc       : 1; // not used after datasheet
+        uint8_t rtt       : 1; // not used after datasheet
+        uint8_t wdg       : 1; // not used after datasheet
+        uint8_t pmc       : 1; // not used after datasheet
+        uint8_t efc0      : 1; // not used after datasheet
+        uint8_t efc1      : 1; // not used after datasheet
+        uint8_t dbgu      : 1;
+        uint8_t hsmc4     : 1;
+        uint8_t pioa      : 1;
+        uint8_t piob      : 1;
+        uint8_t pioc      : 1;
+        uint8_t us0       : 1;
+        uint8_t us1       : 1;
+        uint8_t us2       : 1;
+        uint8_t us3       : 1;
+        uint8_t mci0      : 1;
+        uint8_t twi0      : 1;
+        uint8_t twi1      : 1;
+        uint8_t spi0      : 1;
+        uint8_t ssc0      : 1;
+        uint8_t tc0       : 1;
+        uint8_t tc1       : 1;
+        uint8_t tc2       : 1;
+        uint8_t pwmc      : 1;
+        uint8_t adc12b    : 1;
+        uint8_t adc       : 1;
+        uint8_t hdma      : 1;
+        uint8_t udphs     : 1;
+        uint8_t pid30     : 1; // not used on sam3u
+        uint8_t pid31     : 1; // not use don sam3u
+    } __attribute__((__packed__)) bits;
+} pmc_pcsr_t;
+
+/**
+ * PMC UTMI Clock Configuration Register, AT91 ARM Cortex-M3 based Microcontrollers
+ * SAM3U Series, Preliminary, p. 488
+ */
+typedef union
+{
+    uint32_t flat;
+    struct
+    {
+        uint16_t reserved0: 16;
+        uint8_t upllen    :  1; // UTMI PLL enable
+        uint8_t reserved1 :  3;
+        uint8_t upllcount :  4; // UTMI PLL Start-up Time (in number of slow clock cycles times 8
+        uint8_t reserved2 :  8; 
+    } __attribute__((__packed__)) bits;
+} pmc_uckr_t;
+
+typedef struct
+{
+    volatile pmc_pcer_t pcer; // Peripheral Clock Enable Register
+    volatile pmc_pcdr_t pcdr; // Peripheral Clock Disable Register
+    volatile pmc_pcsr_t pcsr; // Peripheral Clock Status Register
+} pmc_pc_t;
+
 /**
  * PMC Register definitions, AT91 ARM Cortex-M3 based Microcontrollers SAM3U
  * Series, Preliminary, p. 481
